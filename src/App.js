619ec1c07b026af2bd8136f5e64d476d1fccca54
Mateus Faissal <mateusfaissal@gmail.com>
2023-05-15 15:08:36 -0300

    Versao com data-tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
+import { useState } from 'react';
 import Jogo from './Jogo';
 import Letras from './Letras';
-import Palavras from './palavras';
-
-
+import palavras from './palavras';
 
 
 function App() {
+  let [word, setWord] = useState('');
+  let [wordLetters, setWordLetters] = useState([]);
+  let [showWord, setShowWord] = useState([]);
+  let [habilitaBtn, setHabilitaBtn] = useState(true);
+  let [errors, setErrors] = useState(0);
+  let [result, setResult] = useState('');
+
+
   return (
     <div className='container-game'>
-      <Jogo />
-      <Letras />
+      <Jogo
+        palavras={palavras}
+        word={word}
+        setWord={setWord}
+        wordLetters={wordLetters}
+        setWordLetters={setWordLetters}
+        showWord={showWord}
+        setShowWord={setShowWord}
+        habilitaBtn={habilitaBtn}
+        setHabilitaBtn={setHabilitaBtn}
+        errors={errors}
+        setErrors={setErrors}
+        result={result}
+        setResult={setResult}
+      />
+      <Letras
+        habilitaBtn={habilitaBtn}
+        setHabilitaBtn={setHabilitaBtn}
+        wordLetters={wordLetters}
+        showWord={showWord}
+        setShowWord={setShowWord}
+        errors={errors}
+        setErrors={setErrors}
+      />
     </div>
   );
 }
